e3aac02dc9ede3cae71b6864806ab3f6e866679f
srividyapasangulapati <srividyapasangulapati@gmail.com>
2025-12-10 15:26:35 +0530

    Add files via upload

---
+++ DataTypes.java
@@ -0,0 +1,26 @@
+public class DataTypes {
+    public static void main(String[] args) {
+        int id = 101;
+        String name = "Srividya";
+        int  age = 19;
+        String bloodGroup = "O+";
+        String department = "Computer Science";
+        String block = "SOE 3";
+        char grade = 'A';
+        double gpa = 8.65;
+        float salary = 75000.50f;
+        boolean isPassed = true;
+        byte byteValue = 100;
+        System.out.println("ID: " + id);
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Blood Group: " + bloodGroup);
+        System.out.println("Department: " + department);
+        System.out.println("Block: " + block);
+        System.out.println("Grade: " + grade);
+        System.out.println("GPA: " + gpa);
+        System.out.println("Is Passed: " + isPassed);
+        System.out.println("Salary: " + salary);
+        System.out.println("Byte Value: " + byteValue);
+    }
+}
---
+++ Variables.java
@@ -0,0 +1,18 @@
+class Variables {
+    public static void main(String[] args) {
+        int id = 101;
+        String name = "Srividya";
+        String department = "Computer Science";
+        String block = "SOE 3";
+        String bloodGroup = "O+";
+        float Salary = 75000.50f;
+        double gpa = 8.65;
+        System.out.println("ID: " + id);
+        System.out.println("Name: " + name);
+        System.out.println("Department: " + department);
+        System.out.println("Block: " + block);
+        System.out.println("Blood Group: " + bloodGroup);
+        System.out.println("Salary: " + Salary);
+        System.out.println("GPA: " + gpa);
+    }
+}
